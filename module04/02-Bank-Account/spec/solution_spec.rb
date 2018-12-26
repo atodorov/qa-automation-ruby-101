10430451a1594b1c3c0389ade66671d9adad6404
Alexander Todorov <atodorov@redhat.com>
2018-12-26 23:50:58 +0200

    Add problem 02-Bank-Account

---
+++ module04/02-Bank-Account/spec/solution_spec.rb
@@ -0,0 +1,67 @@
+require 'solution'
+
+describe BankAccount do
+    before(:each) do
+        @account = BankAccount.new("Rado", 0, "$")
+    end
+
+    it 'can create a bank account' do
+        expect(@account.is_a?(BankAccount)).to be true
+    end
+
+    it '.initiallize() raises error if amount is negative' do
+        expect { BankAccount.new("Test", -100, "$") }.to raise_error(ArgumentError)
+    end
+
+    it '.initiallize() raises error if currency not set' do
+        expect { BankAccount.new("Test", 100, "") }.to raise_error(ArgumentError)
+    end
+
+    it '.deposit() raises error if amount is negative' do
+        expect { @account.deposit(-10) }.to raise_error(ArgumentError)
+    end
+
+    it '.deposit() in empty account works' do
+        @account.deposit(500)
+        expect(@account.balance).to eq(500)
+    end
+
+    it '.deposit() in non-empty account works' do
+        account = BankAccount.new("Ivo", 1000, "$")
+        account.deposit(500)
+        expect(account.balance).to eq(1500)
+    end
+
+    it '.balance is a Numeric' do
+        expect(@account.balance.is_a?(Numeric)).to be true
+    end
+
+    it '.withdraw() from non-empty account works' do
+        @account.deposit(100)
+        result = @account.withdraw(50)
+
+        expect(result).to be true
+        expect(@account.balance).to eq(50)
+    end
+
+    it '.withdraw() from empty-account works per specification' do
+        result = @account.withdraw(50)
+
+        expect(result).to be false
+        expect(@account.balance).to eq(0)
+    end
+
+    it '.history works per specification' do
+        account = BankAccount.new("Test", 0, "$")
+        account.deposit(20)
+        account.balance
+        expected = ["Account was created", "Deposited 20$", "Balance check -> 20$"]
+
+        expect(account.history).to eq(expected)
+    end
+
+    it '.to_s() should work per specification' do
+        @account.deposit(22)
+        expect(@account.to_s).to eq("Bank account for Rado with balance of 22$")
+    end
+end
